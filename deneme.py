3e90f147a0436f54bff16ff18db2ba8d76738d37
Bilgin Aksoy <bilginaksoy2003@yahoo.com>
2018-01-09 20:16:55 +0300

    T2 found.

--- deneme.py
+++ deneme.py
@@ -1,11 +1,20 @@
-import pandas as pd
 import math
 
-huff_t1_df = pd.read_csv('./huff_t1.csv')
+amplitude = 3
+size = int(math.log(abs(amplitude), 2)) + 1
 
-amp = -15
+print(size)
+code = None
+if size == 0:
+    code = None
+else:
+    if amplitude > 0:
+        to_code = pow(2, size - 1)
+        code = bin((amplitude - to_code) | (1 << (size - 1)))
+    else:
+        to_code = pow(2, size - 1)
+        code = bin(abs(amplitude - to_code) | (0 << (size - 1)))
+print(code)
+bytearrays=bytearray(code)
 
-size = int(math.log(abs(amp), 2))+1
-
-# print(size)
-print(huff_t1_df['Code'][size])
+print(bytearrays)
